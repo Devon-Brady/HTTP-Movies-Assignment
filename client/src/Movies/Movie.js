b448a974d17f40334bf96999da862d122777287e
Devon-brady <devon.a.brady@gmail.com>
2020-12-16 18:51:03 -0500

    update movie

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams,Route, NavLink ,useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie({ props, addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
-
+  const {push} = useHistory();
   const fetchMovie = (id) => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then((res) => setMovie(res.data))
       .catch((err) => console.log(err.response));
   };
-
+ console.log(movie);
   const saveMovie = () => {
     addToSavedList(movie);
   };
@@ -33,6 +33,11 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button onClick={()=>{
+        push(`/update-movie/${movie.id}`)
+      }}>
+        Update
+      </button>
     </div>
   );
 }
